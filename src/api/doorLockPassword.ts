60ef2ee4e0aeac0ba5977b2c9cac3bff964e885e
Coalery <doralife12@gmail.com>
2024-11-16 00:24:16 +0900

    feat: react-query 세팅 및 도어락 API 호출부 정의

---
+++ src/api/doorLockPassword.ts
@@ -0,0 +1,30 @@
+import apiClient from "./client";
+
+export type GetDoorLockPasswordResponseDto = {
+  master: string;
+  forJajudy: string;
+  forFacilityTeam: string;
+};
+
+export type UpdateDoorLockPasswordRequestDto = {
+  master: string;
+  forJajudy: string;
+  forFacilityTeam: string;
+};
+
+export const getDoorLockPassword = async () => {
+  const response = await apiClient.get<GetDoorLockPasswordResponseDto>(
+    "/manager/door-lock-password"
+  );
+  return response.data;
+};
+
+export const updateDoorLockPassword = async (
+  request: UpdateDoorLockPasswordRequestDto
+) => {
+  const response = await apiClient.put<GetDoorLockPasswordResponseDto>(
+    "/manager/door-lock-password",
+    request
+  );
+  return response.data;
+};
