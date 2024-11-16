3a298af9b4142c772cb59eb65b6b58469cc95ec1
Coalery <doralife12@gmail.com>
2024-11-17 01:39:37 +0900

    feat: 라우터 적용

--- README.md
+++ README.md
@@ -24,3 +24,24 @@ $ yarn run dev
 ```sh
 $ yarn run build
 ```
+
+## 디렉토리 구조
+
+```
+sight-frontend/
+├── public/
+│   └── ...
+├── src/
+│   ├── api/               # API 호출과 관련된 로직
+│   ├── components/        # 공통 UI 컴포넌트
+│   ├── features/          # 페이지별 주요 기능을 담당하는 모듈
+│   ├── layouts/           # 레이아웃 관련 컴포넌트
+│   ├── pages/             # 라우팅되는 주요 페이지들
+│   ├── util/              # 공통으로 사용되는 유틸리티 함수
+│   ├── App.tsx            # 라우트가 정의된 파일
+│   ├── main.tsx           # 리액트 엔트리포인트
+│   └── ...
+└── package.json
+```
+
+개발 중에 위 디렉토리 구조 외 다른 디렉토리가 존재하는 걸 발견하시면, PR을 통해 수정해주세요!
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
 import InfraBluePage from "./pages/manage/infraBlue";
 
 function App() {
-  return <InfraBluePage />;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/">
+        <Route path="/manage/infra-blue" element={<InfraBluePage />} />
+      </Route>
+    ),
+    {
+      future: {
+        v7_fetcherPersist: true,
+        v7_normalizeFormMethod: true,
+        v7_partialHydration: true,
+        v7_relativeSplatPath: true,
+        v7_skipActionErrorRevalidation: true,
+      },
+    }
+  );
+
+  return (
+    <RouterProvider future={{ v7_startTransition: true }} router={router} />
+  );
 }
 
 export default App;
