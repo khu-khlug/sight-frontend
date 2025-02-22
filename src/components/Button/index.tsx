b49d3da7a6e070cb7789d45c3a34e17de6e3951f
Coalery <doralife12@gmail.com>
2025-02-22 18:04:30 +0900

    fix: 버튼 스타일 수정

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,15 @@
 import { ButtonHTMLAttributes } from "react";
 import { cn } from "../../util/cn";
 
-import "./style.css";
+import styles from "./style.module.css";
+
+type Variant = "primary" | "neutral";
 
 type Props = {
   children?: React.ReactNode;
   className?: string;
   type?: ButtonHTMLAttributes<HTMLButtonElement>["type"];
+  variant?: Variant;
   disabled?: boolean;
   onClick?: () => void;
 };
@@ -15,13 +18,14 @@ export default function Button({
   children,
   className,
   type,
+  variant = "primary",
   disabled = false,
   onClick,
 }: Props) {
   return (
     <button
       type={type}
-      className={cn("component-button", className)}
+      className={cn(styles["component-button"], styles[variant], className)}
       disabled={disabled}
       onClick={onClick}
     >
