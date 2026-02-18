8b1488ff36fcca7e2a3278826bfd04c257d851ce
Coalery <doralife12@gmail.com>
2026-02-18 22:52:32 +0900

    fix: 메인 페이지 레거시 배너 카피라이팅 수정

--- src/features/main/LegacySiteBanner/index.tsx
+++ src/features/main/LegacySiteBanner/index.tsx
@@ -5,11 +5,16 @@ export default function LegacySiteBanner() {
   return (
     <Box mt={4} mb={4}>
       <Callout type="warning">
-        새로운 사이트입니다. 문제가 있다면{" "}
-        <Link href="https://khlug.org" color="orange.600" fontWeight="bold" target="_blank">
-          이전 사이트
+        쿠러그의 새로운 메인 페이지에요. 이전 사이트로 가고 싶다면{" "}
+        <Link
+          href="https://khlug.org"
+          color="orange.600"
+          fontWeight="bold"
+          target="_blank"
+        >
+          여기
         </Link>
-        를 이용해주세요.
+        를 눌러주세요.
       </Callout>
     </Box>
   );
