5ced81eae21fe78e942327d5dc8ff6b487ebaf28
nananina <nananina0415@gmail.com>
2026-03-10 16:44:57 +0900

    feat: PWA로 설치 가능

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,43 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest: {
+          name: "쿠러그",
+          short_name: "쿠러그",
+          description:
+            "경희대학교 중앙 IT 동아리, 세상의 모든 ICT 기술을 다룹니다.",
+          theme_color: "#ffffff",
+          background_color: "#ffffff",
+          display: "standalone",
+          start_url: "/",
+          icons: [
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+      }),
+    ],
+    preview: {
+      host: "0.0.0.0",
+    },
     define: {
       "process.env": env,
     },
