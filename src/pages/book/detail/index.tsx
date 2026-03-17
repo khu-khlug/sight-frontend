e5c1c914732766e4d1422f4b680b612a799aa244
nananina <nananina0415@gmail.com>
2026-03-17 21:50:01 +0900

    feat: 도서 상세 페이지 구현
    표지, 저자/출판사/ISBN 정보, 소장 목록, 대출/반납 버튼 포함.

--- src/pages/book/detail/index.tsx
+++ src/pages/book/detail/index.tsx
@@ -1,1 +1,14 @@
-export default function BookDetailPage() { return null; }
+import { useParams } from "react-router-dom";
+import MainLayout from "../../../layouts/MainLayout";
+import BookNavBar from "../../../features/book/BookNavBar";
+import BookDetailContainer from "../../../features/book/BookDetailContainer";
+
+export default function BookDetailPage() {
+  const { bookId } = useParams<{ bookId: string }>();
+  return (
+    <MainLayout>
+      <BookNavBar />
+      <BookDetailContainer bookId={bookId!} />
+    </MainLayout>
+  );
+}
