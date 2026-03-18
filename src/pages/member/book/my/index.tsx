2b793714dd818a528b77787291b34fc5353b5bb5
nananina <nananina0415@gmail.com>
2026-03-18 14:59:43 +0900

    refactor: 내 대출 페이지를 feat/book-my로 분리

--- src/pages/member/book/my/index.tsx
+++ src/pages/member/book/my/index.tsx
@@ -1,12 +1,1 @@
-import MainLayout from "../../../../layouts/MainLayout";
-import BookNavBar from "../../../../features/book/BookNavBar";
-import BookMyBorrowingContainer from "../../../../features/book/BookMyBorrowingContainer";
-
-export default function BookMyPage() {
-  return (
-    <MainLayout>
-      <BookNavBar current="my" />
-      <BookMyBorrowingContainer />
-    </MainLayout>
-  );
-}
+export default function BookMyPage() { return null; }
