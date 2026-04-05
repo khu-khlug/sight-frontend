5fa3af043ef3c7da31df29c588082fa0fb255a8c
구자웅 <nananina0415@gmail.com>
2026-04-05 23:56:40 +0900

    feat: PWA 및 브라우저용 아이콘 추가 (#13)
    * feat: PWA 아이콘 변경
    
    가시성이 좋은 아이콘으로 변경.
    realfavicongenerator.net에서 제작함.
    
    * feat: PWA 매니페스트 lang, maskable 아이콘 추가
    
    * feat: 하얀색 바탕 아이콘셋 추가
    
    * chore: 아이콘 리소스 커밋
    
    * feart: PWA 캐싱 변경
    바코드 인식 라이브러리를 서비스워커 캐시에서 제외.
    자주 쓰지않는 기능이라 단기간 브라우저 캐싱으로 중분함.
    
    * fix: 일반 아이콘설정이 빠진것을 수정

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig(({ mode }) => {
       react(),
       VitePWA({
         registerType: "autoUpdate",
+        workbox: {
+          globIgnores: ["assets/zbar-*.wasm"],
+        },
         manifest: {
           name: "쿠러그",
           short_name: "쿠러그",
@@ -18,18 +21,21 @@ export default defineConfig(({ mode }) => {
             "경희대학교 중앙 IT 동아리, 세상의 모든 ICT 기술을 다룹니다.",
           theme_color: "#ffffff",
           background_color: "#ffffff",
+          lang: "ko",
           display: "standalone",
           start_url: "/",
           icons: [
             {
-              src: "pwa-192x192.png",
+              src: "icon/pwa-192x192.png",
               sizes: "192x192",
               type: "image/png",
+              purpose: "any maskable",
             },
             {
-              src: "pwa-512x512.png",
+              src: "icon/pwa-512x512.png",
               sizes: "512x512",
               type: "image/png",
+              purpose: "any maskable",
             },
           ],
         },
